d56febe3a37e987ddbaa6e12301014acc9588072
Udit Kumar Agrawal <2001uditagrawal@gamil.com>
2023-03-07 15:54:33 +0530

    added nultiply.jqvq'

---
+++ multiply.java
@@ -0,0 +1,8 @@
+package letsLearnGit;
+
+public class multiply {
+    public static void main(String[] args) {
+        System.out.println(3*5);
+    }
+    
+}
--- sum.java
+++ sum.java
@@ -3,6 +3,6 @@ package letsLearnGit;
 public record sum() {
     public static void main(String[] args) {
         int a=5, b=10;
-        System.out.println("sum =" + a+b);
+        System.out.println("sum of any no is =" + a+b);
     }
 }
